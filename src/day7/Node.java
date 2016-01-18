14b8adfabe9a40fdf42c4fafef18cc477032188c
acsent <acsent@list.ru>
2016-01-18 22:40:04 +0300

    day7.2

--- src/day7/Node.java
+++ src/day7/Node.java
@@ -6,12 +6,14 @@ import java.util.Set;
 public class Node {
     String name;
     Object[] input;
+    Integer[] calculatedInput;
     String operation;
     Integer value;
     Set<Node> parents;
 
     public Node() {
         this.input = new Object[2];
+        this.calculatedInput = new Integer[2];
         this.parents  = new HashSet<>();
     }
 
